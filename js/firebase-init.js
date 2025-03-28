63ee79e39884eb2da91da73b9fd427f09112b3cc
hyogiman <77236186+hyogiman@users.noreply.github.com>
2025-03-28 22:25:14 +0900

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "🔥 your-api-key",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "xxxx",
+  appId: "xxxx"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
