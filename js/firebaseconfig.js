8c8a24c07841b429cf3f1d68a6ab96ab851da30a
hyogiman <77236186+hyogiman@users.noreply.github.com>
2025-03-28 22:26:16 +0900

    Update firebaseconfig.js

--- js/firebaseconfig.js
+++ js/firebaseconfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "🔥 your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "xxxx",
-  appId: "xxxx"
+  apiKey: "AIzaSyBhI_N0bPlhaBaNXiyIHHWX5KzIznPD9fs",
+  authDomain: "leadershipg-v1.firebaseapp.com",
+  projectId: "leadershipg-v1",
+  storageBucket: "leadershipg-v1.firebasestorage.app",
+  messagingSenderId: "1069669847032",
+  appId: "1:1069669847032:web:db299884387f59c58b500d"
 };
 
 const app = initializeApp(firebaseConfig);
